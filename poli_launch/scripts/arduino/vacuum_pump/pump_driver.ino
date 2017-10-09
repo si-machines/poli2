d153e0907449987b4f9fe084fc20661677bd6766
maxsvetlik <maxsvetlik@gmail.com>
2017-10-09 16:45:23 -0500

    ros-ified pump driver- untested

--- poli_launch/scripts/arduino/vacuum_pump/pump_driver.ino
+++ poli_launch/scripts/arduino/vacuum_pump/pump_driver.ino
@@ -1,3 +1,17 @@
+
+/*
+ * Driver that controls the de/activation of an electric pump
+ * Used with a DualMC33926 Motor controller.
+ *
+ */
+
+
+#include <ros.h>
+#include <ros/time.h>
+#include <std_msgs/Int16.h>
+#include <actionlib/server/simple_action_server.h>
+#include <poli_msgs/GripperPumpAction.h>
+
 #include "DualMC33926MotorShield.h"
 
 DualMC33926MotorShield md;
@@ -6,41 +20,73 @@ bool flag1 = false;
 bool flag2 = false;
 int status = 0;
 
+class GripperPumpAction
+{
+protected:
+
+  ros::NodeHandle nh_;
+  actionlib::SimpleActionServer<poli_msgs::GripperPumpAction> as_; // NodeHandle instance must be created before this line. Otherwise strange error occurs.
+  std::string action_name_;
+  // create messages that are used to published feedback/result
+  poli_msgs::GripperPumpFeedback feedback_;
+  poli_msgs::GripperPumpResult result_;
+  poli_msgs::GripperPumpAction gpa;
+
+public:
+
+  GripperPumpAction(std::string name) :
+    as_(nh_, name, boost::bind(&GripperPumpAction::executeCB, this, _1), false),
+    action_name_(name)
+  {
+    nh.initNode();
+    as_.start();
+  }
+
+  void executeCB(const poli_msgs::GripperPumpGoalConstPtr &goal)
+  {
+    // helper variables
+    ros::Rate r(1);
+    bool success = true;
+
+    // publish info to the console for the user
+    ROS_INFO("Executing gripper pump action...");
+
+    // start executing the action
+    if(goal->command == gpa.ACTIVATE){
+      md.setM1Speed(400);
+    }
+    else if(goal->command == gpa.DEACTIVATE){
+      md.setM1Speed(0);
+    }
+    else if(goal->command == gpa.RELEASE){
+      md.setM2Speed(400);
+      delay(100);
+      md.setM2Speed(0);
+      flag2 = false;
+    }
+
+
+    //md.GetFault();
+    result.success = gpa.SUCCESS;
+    ROS_INFO("%s: Succeeded", action_name_.c_str());
+    // set the action state to succeeded
+    as_.setSucceeded(result_);
+
+  }
+};
+
+
+
 void stopIfFault()
 {
   if (md.getFault())
   {
+    ROS_ERROR("Gripper pump driver faulted.");
+    ROS_ERROR("Shutting down pump driver.");
     Serial.println("fault");
     while(1);
   }
 }
-
-void letitgo()
-{
-  if(flag2){
-    Serial.println("Releasing...");
-    
-    md.setM2Speed(400);
-    delay(100);
-    md.setM2Speed(0);
-    flag2 = false;
- }
-}
-
-void grabitpls()
-{
-  if(flag1)
-  {
-    Serial.println("Grabber Engaged");
-    md.setM1Speed(400);
-  }
-  else
-  {
-    Serial.println("Grabber Disengaged");
-    md.setM1Speed(0);
-  }
-}
-
 
 void setup() {
 
@@ -53,32 +99,6 @@ void setup() {
 }
 
 void loop() {
-  
-   while (Serial.available() > 0) {
-
-    // look for the next valid integer in the incoming serial stream:
-    status = Serial.parseInt();
-   
-
-    if (status ==1)
-    {
-      flag1 = true;
-      flag2 = false;
-    }
-    else if ((status ==2))
-    {
-      flag2=true;  
-    }
-    else
-    {
-      flag1 = false;
-      flag2 = false;
-    }
-
-    grabitpls();
-    letitgo();
-   }    
-   } 
-  
-
+   GripperPumpAction gripperPump("gripperpump");
+   ros::spin();
 
