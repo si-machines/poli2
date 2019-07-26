d1263678fd417637e3b0054e39098f88d9d899f1
lupe <lupe@lupe1>
2019-07-26 15:08:59 -0500

    Add aliases for setting robots as ROS master

--- setup/shortcuts.bash
+++ setup/shortcuts.bash
@@ -20,3 +20,38 @@ alias force_control_stop='rosservice call /j2s7s300_driver/in/stop_force_control
 # Gravity Compensation
 alias grav_comp_start='rosservice call /j2s7s300_driver/in/start_gravity_comp'
 alias grav_comp_stop='rosservice call /j2s7s300_driver/in/stop_gravity_comp'
+
+# Setting various robots as master
+ip_mine=$(ifconfig `cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1'` | grep -oP 'inet addr:\K\S+')
+
+ip_moe1="10.66.171.91"
+ip_lupe1="10.145.171.181"
+# TODO: set barton1's IP address when it's known (i.e. when you try to run this on Barton)
+ip_barton1="0.0.0.0"
+
+set_me_master() {
+  export ROS_MASTER_URI="http://localhost:11311"
+  export ROS_HOSTNAME="localhost"
+  export ROS_IP=$ip_mine
+}
+
+set_moe_master () {
+  export ROS_MASTER_URI="http://${ip_moe1}:11311"
+  export ROS_HOSTNAME="moe1"
+  export ROS_IP=$ip_mine
+}
+
+set_lupe_master () {
+  export ROS_MASTER_URI="http://${ip_lupe1}:11311"
+  export ROS_HOSTNAME="lupe1"
+  export ROS_IP=$ip_mine
+}
+
+set_barton_master () {
+  echo "ERROR: THIS IP ADDRESS IS NOT KNOWN YET"
+  echo "UPDATE poli2/setup/shortcuts.bash WITH THE CORRECT"
+  echo "IP ADDRESS FOR BARTON ONE, THEN COMMIT AND PUSH THE CHANGE."
+  export ROS_MASTER_URI="http://${ip_barton1}:11311"
+  export ROS_HOSTNAME="barton1"
+  export ROS_IP=$ip_mine
+}
