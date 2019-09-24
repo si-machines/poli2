80e8606734f79187cef2d8bda918327298a66519
Adam Allevato <Kukanani@users.noreply.github.com>
2019-09-24 10:37:34 -0500

    Tablebot with spatula added

--- poli2_launch/scripts/setup_tablebot_collision_scene.py
+++ poli2_launch/scripts/setup_tablebot_collision_scene.py
@@ -2,21 +2,21 @@
 
 # Copyright (c) 2017, Elaine Short, SIM Lab
 # All rights reserved.
-#
+# 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-#
+# 
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
-#
+# 
 # * Redistributions in binary form must reproduce the above copyright notice,
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
-#
+# 
 # * Neither the name of the SIM Lab nor the names of its
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
-#
+# 
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -31,21 +31,45 @@
 import rospy
 from planning_scene_helper import PlanningSceneHelper
 
-HIGH_TABLE_Z = -0.135
-TABLE_Y = -0.6
-LOW_TABLE_OFFSET = 0.320675
-WALL_COLOR = (0,0,1.,0.4)
-TABLE_COLOR = (0,0,1.,0.4)
+def m(inch):
+    """meters from inches"""
+    conv = 0.0254
+    if type(inch) in (tuple, list) :
+        return [x*conv for x in inch]
+    return inch*conv
 
-rospy.init_node("planning_scene_setup")
-p = PlanningSceneHelper()
+if __name__=="__main__":
+    rospy.init_node("planning_scene_setup")
+    p = PlanningSceneHelper()
 
-p.remove("table")
-p.remove("back")
-p.remove("low_table")
-p.remove("hoop_protector")
+    p.remove("table")
+    p.remove("back")
+    p.remove("camera_right_tower")
+    p.remove("camera_left_tower")
+    p.remove("camera_top_bar")
+    p.remove("roof")
 
-p.add_box("table", "world", size=(0.7, 1.25, 0.2), position=(0, TABLE_Y, HIGH_TABLE_Z), color=TABLE_COLOR)
-p.add_box("back", "world", size=(2, 0.2, 2), position=(0, 0.22, 0), color=WALL_COLOR)
-# p.add_box("low_table", "world", size=(0.7, 1.25, 0.2), position=(-0.7, TABLE_Y, HIGH_TABLE_Z+LOW_TABLE_OFFSET), color=TABLE_COLOR)
-p.add_box("hoop_protector", "world", size=(0.2, 2, 2), position=(-0.9, 0, 0), color=WALL_COLOR)
+    table_z = -0.04
+    table_thickness = 0.2
+    height = m(27.75)
+    post_xsize = m(3)
+    post_ysize = m(3)
+    foot_ysize = m(15)
+    foot_xsize = m(3)
+    foot_zsize = m(3)
+    # make it much taller than strictly necessary just so we never pass in front of and above the crossbar
+    top_bar_thickness = 0.3
+    bar_pos = m(-22)
+
+    safety_buffer = 0.04
+
+    p.add_box("back", "world", size=(2, 0.1, 2), position=(0, 0.35, 0), color=(0,0,1.,0.4))
+    p.add_box("table", "world", size=(2, 2, table_thickness), position=(0, 0, table_z - table_thickness/2), color=(0,0,1.,0.4))
+    p.add_box("roof", "world", size=(2, 2, table_thickness), position=(0, 0, 0.85 + table_thickness/2), color=(0, 0, 0, 0.4))
+
+    p.add_box("camera_right_foot", "world", position=(m(27.5/2), bar_pos, table_z + foot_zsize/2), size=(foot_xsize, foot_ysize, foot_zsize), color=(1,1,0,0.4))
+    p.add_box("camera_left_foot", "world", position=(-m(27.5/2), bar_pos, table_z + foot_zsize/2), size=(foot_xsize, foot_ysize, foot_zsize), color=(1,1,0,0.4))
+
+    p.add_box("camera_right_tower", "world", position=(m(27.5/2), bar_pos, table_z + height/2), size=(post_xsize, post_ysize + 0.01, height), color=(1,1,0,0.4))
+    p.add_box("camera_left_tower", "world", position=(-m(27.5/2), bar_pos, table_z + height/2), size=(post_xsize, post_ysize + 0.01, height), color=(1,1,0,0.4))
+    p.add_box("camera_top_bar", "world", position=(0, bar_pos, table_z + height + top_bar_thickness/2 - safety_buffer), size=(m(36), m(4), top_bar_thickness), color=(1, 1, 0, 0.4))
