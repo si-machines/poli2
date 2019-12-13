64be8e8cb5e878ff1c6a43fc129269c365554c3d
Adam Allevato <Kukanani@users.noreply.github.com>
2019-12-13 13:58:16 -0600

    Added gravity estimation program to util folder

---
+++ util/gravity_estimation/CMakeFiles/gravestim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gravestim.dir/main.cpp.o"
+  "gravestim.pdb"
+  "gravestim"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gravestim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
